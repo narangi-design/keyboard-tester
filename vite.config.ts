88f84eab3ec30add5c6f846604146caa71e1d97e
Daria Ryabova <dashagross111@gmail.com>
2025-12-01 15:29:39 +0100

    feat: deployed on github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/keyboard-tester/',
   server: {
     host: "0.0.0.0",
   },
